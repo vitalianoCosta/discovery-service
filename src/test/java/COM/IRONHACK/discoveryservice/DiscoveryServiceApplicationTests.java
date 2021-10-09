310167cd755b9a3fc79f055f915205b074fc1bc8
vitalianoCosta <84536802+vitalianoCosta@users.noreply.github.com>
2021-10-09 11:44:00 +0100

    Initial commit

---
+++ src/test/java/COM/IRONHACK/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package COM.IRONHACK.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
